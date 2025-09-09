3d5810c091b550cd406a9ebd2d202224bdf78df8
Sami Awadh <eng.sami.cyber@gmail.com>
2025-09-09 07:55:24 +0200

    Add FileService for file management functionality
    Introduces a new `FileService` class implementing the `IFileService` interface.
    The `SaveFile` method handles file saving with extension validation and unique naming,
    while the `DeleteFile` method allows for file removal with error handling.
    The `IFileService` interface is defined to outline the required methods for file operations.

---
+++ SplendoreMVP/Services/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace SplendoreMVP.Services
+{
+    public interface IFileService
+    {
+        void DeleteFile(string fileName);
+        Task<string> SaveFile(IFormFile file, string[] allowedExtensions);
+    }
+}
